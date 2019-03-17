6e43b7511dc3825325672da4de3268d7277b694d
NemanjaSimic <snemanja.simic@gmail.com>
2019-03-17 23:43:10 +0100

    Inicijalni dizajn

---
+++ Service/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Service.ViewModels;
+
+namespace Service
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		#region DataContext
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void ZaposleniButton_Click(object sender, RoutedEventArgs e)
+		{
+			DataContext = new ZaposleniViewModel();
+		}
+
+		private void MrezeButton_Click(object sender, RoutedEventArgs e)
+		{
+			DataContext = new MrezeViewModel();
+		}
+
+		private void KorisniciButton_Click(object sender, RoutedEventArgs e)
+		{
+			DataContext = new KorisniciViewModel();
+		}
+
+		private void EkipeButton_Click(object sender, RoutedEventArgs e)
+		{
+			DataContext = new EkipeViewModel();
+		}
+
+		private void RadniNalogButton_Click(object sender, RoutedEventArgs e)
+		{
+			DataContext = new RadniNalogViewModel();
+		}
+
+		private void MagacinButton_Click(object sender, RoutedEventArgs e)
+		{
+			DataContext = new MagacinViewModel();
+		}
+
+		private void DeoOpremeButton_Click(object sender, RoutedEventArgs e)
+		{
+			DataContext = new DeoOpremeViewModel();
+		}
+
+		private void LokacijaButton_Click(object sender, RoutedEventArgs e)
+		{
+			DataContext = new LokacijaViewModel();
+		}
+
+		private void KvaroviButton_Click(object sender, RoutedEventArgs e)
+		{
+			DataContext = new KvarViewModel();
+		}
+
+		private void RacuniButton_Click(object sender, RoutedEventArgs e)
+		{
+			DataContext = new RadniNalogViewModel();
+		}
+		#endregion
+
+
+	}
+}
